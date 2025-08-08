c8e57c635154afff7149ee6d7101a2caf7562bf3
birdsofprey97sherrif <birdsofpreysherrif@gmail.com>
2025-08-08 11:25:26 +0300

    note

---
+++ utils/notifications.js
@@ -0,0 +1,9 @@
+// src/utils/notifications.js
+
+const sendNotification = (to, message) => {
+  console.log(`Sending notification to ${to}: ${message}`);
+};
+
+module.exports = {
+  sendNotification,
+};
